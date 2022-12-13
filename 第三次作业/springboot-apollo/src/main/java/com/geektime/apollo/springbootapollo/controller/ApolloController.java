e8add257b46bb5b12bb52f77615b956fe5af7ac3
邬锦华 <jinhua.wu1@gientech.com>
2022-12-13 23:00:13 +0800

    update

--- 第三次作业/springboot-apollo/src/main/java/com/geektime/apollo/springbootapollo/controller/ApolloController.java
+++ 第三次作业/springboot-apollo/src/main/java/com/geektime/apollo/springbootapollo/controller/ApolloController.java
@@ -9,36 +9,50 @@ import org.springframework.web.bind.annotation.*;
 public class ApolloController {
 
 
-//	@Value ("${name}")
-//	private String name;
-//	@Value ("${code}")
-//	private String code;
-//
-//
-//	@GetMapping("apollo1")
-//	public void testApollo1(){
-//		log.info ("name: {}",name);
-//		log.info ("code: {}",code);
-//	}
+	@Value ("${name:AA}")
+	private String name;
+	@Value ("${sex:F}")
+	private String sex;
+
+	@Value ("${age:13}")
+	private String age;
 
 	/**
-	 *
+	 * Placeholder方式
+	 */
+
+	@GetMapping("apollo1")
+	public void testApollo1(){
+		log.info ("name: {}",name);
+		log.info ("sex: {}",sex);
+		log.info ("age: {}",age);
+	}
+
+	/**
+	 * 通过API调用
 	 */
 	@GetMapping("apollo2")
 	public void testApollo2(){
 		Config config = ConfigService.getAppConfig();
 		String name1 = config.getProperty("name", "");
-		String code1 = config.getProperty("code", "");
+		String age = config.getProperty("age", "");
+		String sex = config.getProperty("sex", "");
 		log.info ("name: {}",name1);
-		log.info ("code: {}",code1);
+		log.info ("age: {}",age);
+		log.info ("sex: {}",sex);
+
 	}
 
+	/**
+	 * yaml/yml格式的namespace
+	 */
 	@GetMapping("apollo3")
 	public void testApollo3(){
-		Config appnamespace = ConfigService.getConfig("application");
-		String name2 = appnamespace.getProperty("name2", "");
-		String code2 = appnamespace.getProperty("code2", "");
-		log.info ("name: {}",name2);
-		log.info ("code: {}",code2);
+		Config config = ConfigService.getConfig("testspace.yaml");
+		String property = config.getProperty ("mybatis-plus.mapper-locations", "");
+		String property2 = config.getProperty ("mybatis-plus.global-config.db-config.id-type", "");
+		log.info ("property: {}",property);
+		log.info ("property2: {}",property2);
+
 	}
 }
