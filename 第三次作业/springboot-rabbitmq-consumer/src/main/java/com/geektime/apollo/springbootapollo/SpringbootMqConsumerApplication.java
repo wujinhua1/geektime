b734fd605b9037b679838c55d1de05a2d54d0392
邬锦华 <jinhua.wu1@gientech.com>
2022-12-15 00:39:30 +0800

    update

---
+++ 第三次作业/springboot-rabbitmq-consumer/src/main/java/com/geektime/apollo/springbootapollo/SpringbootMqConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.geektime.apollo.springbootapollo;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+@SpringBootApplication
+public class SpringbootMqConsumerApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run (SpringbootMqConsumerApplication.class, args);
+	}
+
+}
---
+++ 第三次作业/springboot-rabbitmq-provider/src/main/java/com/geektime/apollo/springbootapollo/SpringbootMqProviderApplication.java
@@ -0,0 +1,12 @@
+package com.geektime.apollo.springbootapollo;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+@SpringBootApplication
+public class SpringbootMqProviderApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run (SpringbootMqProviderApplication.class, args);
+	}
+
+}
