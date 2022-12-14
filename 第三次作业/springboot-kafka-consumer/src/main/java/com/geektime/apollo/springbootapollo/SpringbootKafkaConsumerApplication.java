ad5dee8270ed07cc304647dec310d61d8da0ca58
邬锦华 <jinhua.wu1@gientech.com>
2022-12-15 00:40:31 +0800

    add

---
+++ 第三次作业/springboot-kafka-consumer/src/main/java/com/geektime/apollo/springbootapollo/SpringbootKafkaConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.geektime.apollo.springbootapollo;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+@SpringBootApplication
+public class SpringbootKafkaConsumerApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run (SpringbootKafkaConsumerApplication.class, args);
+	}
+
+}
